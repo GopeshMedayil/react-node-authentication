0e4b4e79477df494ae90cb6b9041ff9c138135e7
GopeshMedayil <medayilgopesh@gmail.com>
2022-04-06 23:09:14 +0530

    Added refresh token logic

--- packages/server-api/index.js
+++ packages/server-api/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+
 require('dotenv').config();
 const fsPromises = require('fs').promises;
 const path = require('path');
@@ -9,8 +10,26 @@ const usersDB = {
     users: require('./users.json'),
     setUsers: function (data) { this.users = data }
 }
+const cookieParser = require('cookie-parser');
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
+
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) return res.sendStatus(401);
+    console.log(authHeader); // Bearer token
+    const token = authHeader.split(' ')[1];
+    jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        (err, decoded) => {
+            if (err) return res.sendStatus(403); //invalid token
+            req.user = decoded.username;
+            next();
+        }
+    );
+}
 
 app.post("/auth", async (req, res) => {
     console.log("ksksksk", req.body)
@@ -48,6 +67,32 @@ app.post("/auth", async (req, res) => {
         res.sendStatus(401);
     }
 });
+
+app.get("/refresh", async (req, res) => {
+    console.log("Hello");
+    const cookies = req.cookies;
+    console.log("cookies", cookies)
+    if (!cookies?.jwt) return res.sendStatus(401);
+    const refreshToken = cookies.jwt;
+
+    const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
+    if (!foundUser) return res.sendStatus(403); //Forbidden 
+    // evaluate jwt 
+    jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET,
+        (err, decoded) => {
+            if (err || foundUser.username !== decoded.username) return res.sendStatus(403);
+            const accessToken = jwt.sign(
+                { "username": decoded.username },
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: '30s' }
+            );
+            res.json({ accessToken })
+        }
+    );
+});
+
 
 app.listen(port, (err) => {
     if (err) {
